70da669af2e25bdfbfc1beb4554e7f93b2b84bca
smal1378 <smal137800@gmail.com>
2021-12-29 01:28:04 +0330

    Implement 'ScheduleFCFT' Class In model.py

--- model.py
+++ model.py
@@ -40,3 +40,38 @@ class ScheduleMother:
         """
         raise NotImplemented
 
+
+class ScheduleFCFS(ScheduleMother):
+    def __init__(self):
+        self.processes = []
+        self.gant_output = []
+        self.calc = False  # Whether a simulation has been done or not
+
+    def add_process(self, process: Process):
+        self.processes.append(process)
+
+    def _calc(self):
+        self.gant_output.clear()
+        # we need a simple queue to add processes into it in order of getting in
+        queue: List[Process] = sorted(self.processes, key=lambda e: e.enter)
+        # queue.pop(0) will return first element in queue,
+
+        time = queue[0].enter  # time starts when first process entered
+        while queue:  # until there is process
+            process = queue.pop(0)
+            self.gant_output.append((time, process.name))
+            time += process.calc
+            process.response = time - process.enter  # exit time - enter time
+            process.waiting = time - process.calc - process.enter  # exit - calculate time - enter
+        self.calc = True
+
+    def get_output(self) -> List[Process]:
+        if not self.calc:
+            self._calc()
+        return self.processes.copy()
+
+    def get_gant(self) -> List[Tuple[int, str]]:
+        if not self.calc:
+            self._calc()
+        return self.gant_output.copy()
+
