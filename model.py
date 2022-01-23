80a5457d0b6b0c459c032e913dcf72b34a952673
smal1378 <smal137800@gmail.com>
2022-01-23 15:05:25 +0330

    Implement ScheduleLCFS in model.py

--- model.py
+++ model.py
@@ -357,6 +357,47 @@ class ScheduleFB(ScheduleMother):
         self.gant_chart.append((time, "END"))
 
 
+class ScheduleLCFS(ScheduleMother):
+    name = "LCFS"  # name of scheduler
+
+    def __init__(self):
+        self.processes = []
+        self.gant_output = []
+        self.calc = False  # Whether a simulation has been done or not
+
+    def add_process(self, process: Process):
+        self.processes.append(process)
+        self.calc = False
+
+    def _calc(self):
+        self.gant_output.clear()
+        # we need a simple queue to add processes into it in order of getting in
+        waiting_queue: List[Process] = sorted(self.processes, key=lambda e: e.enter)
+        # queue.pop(0) will return first element in queue,
+        queue: List[Process] = []
+        time = waiting_queue[0].enter  # time starts when first process entered
+        while queue or waiting_queue:  # until there is process
+            while waiting_queue and waiting_queue[0].enter <= time:
+                queue.append(waiting_queue.pop(0))
+            process = queue.pop()
+            self.gant_output.append((time, process.name))
+            time += process.calc
+            process.response = time - process.enter  # exit time - enter time
+            process.waiting = time - process.calc - process.enter  # exit - calculate time - enter
+        self.gant_output.append((time, 'END'))
+        self.calc = True
+
+    def get_output(self) -> List[Process]:
+        if not self.calc:
+            self._calc()
+        return self.processes.copy()
+
+    def get_gant(self) -> List[Tuple[int, str]]:
+        if not self.calc:
+            self._calc()
+        return self.gant_output.copy()
+
+
 def read_from_file(filename: str):
     if not os.path.exists(filename):
         raise FileNotFoundError(f"File Not Found: {filename}")
